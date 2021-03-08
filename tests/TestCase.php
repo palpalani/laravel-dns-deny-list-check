6a969ed88639e38ee74ead07b96a9844362b0d3f
palpalani <palpalani@users.noreply.github.com>
2021-03-08 07:32:58 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace palPalani\LaravelDnsDenyListCheck\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 use palPalani\LaravelDnsDenyListCheck\LaravelDnsDenyListCheckServiceProvider;
 
